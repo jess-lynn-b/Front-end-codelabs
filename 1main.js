8496ea21aa8d776c588dd99114ea08bba36aa880
Jessica Blackwell <138169206+jess-lynn-b@users.noreply.github.com>
2023-09-21 11:15:04 -0500

    Build Real Things in JS from class 9/18
    These are exercises from class 9/18 and building real things to use in future work. Putting everything we have learned so far into action.

---
+++ 1main.js
@@ -0,0 +1,5 @@
+// Exercise 1 Understand DOM Selection
+document.getElementById("myDiv").style.backgroundColor = "lightblue";
+
+console.log(document.getElementById('pgraphs').innertext);
+//Exercise 2 Familiarize with DOM events and inline event handlers in web development
---
+++ dom2main.js
@@ -0,0 +1,17 @@
+// Exercise 2
+function changeBackground(){
+    document.body.style.backgroundColor = 'pink'
+}
+
+// Exercise 4 \\
+// create the button
+function reply_click (){
+    alert ("Button was clicked!");
+}
+
+
+
+
+
+
+
